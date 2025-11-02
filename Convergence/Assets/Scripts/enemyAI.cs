330eb972040f06a613dcadd5f2cf930f7b780ea3
Jeremy Desfosses <jeremydesfosses603@gmail.com>
2025-11-02 13:27:13 -0500

    Added class script

--- Convergence/Assets/Scripts/enemyAI.cs
+++ Convergence/Assets/Scripts/enemyAI.cs
@@ -1,16 +1,89 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class enemyAI : MonoBehaviour
+public class enemyAI : MonoBehaviour, IDamage
 {
+    [SerializeField] NavMeshAgent agent;
+
+    [SerializeField] int HP;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+    [SerializeField] Transform shootPOS;
+
+    [SerializeField] Renderer model;
+
+    Color colorOrig;
+
+    bool playerInTrigger;
+
+    float shootTimer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        colorOrig = model.material.color;
+        gamemanager.instance.updateGameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shootTimer += Time.deltaTime;
+
+        if (playerInTrigger)
+        {
+            agent.SetDestination(gamemanager.instance.player.transform.position);
+
+            if (shootTimer >= shootRate)
+            {
+                shoot();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+        if (HP <= 0)
+        {
+            gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPOS.position, transform.rotation);
     }
 }
