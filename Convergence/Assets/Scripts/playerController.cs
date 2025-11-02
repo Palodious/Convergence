a92a6f20cde1d85b1a1b4d22ae3b7b8c1ca6b6e4
Tomega <tomega821@gmail.com>
2025-11-02 13:39:31 -0500

    began player controller set layer to prevent self damage initilized health and speed setting

--- Convergence/Assets/Scripts/playerController.cs
+++ Convergence/Assets/Scripts/playerController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+    //layer map to player to prevent player form damaging self
+    [SerializeField] LayerMask ignoreLayer;
+
+    [SerializeField] int HP;// so player can have health
+    [SerializeField] int speed; //to give speed setting for player 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
