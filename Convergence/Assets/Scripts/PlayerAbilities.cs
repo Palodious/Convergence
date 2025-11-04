97887a21d3ca010d82b802d24217f60f7d581c48
Teresa Shaver <tmshaver@student.fullsail.edu>
2025-11-03 19:53:02 -0700

    player abilities
    still working out a couple kinks within script

---
+++ Convergence/Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+using System.Collections;
+
+public class playerAbilities : MonoBehaviour
+{
+    [SerializeField] playerController controller;
+    [SerializeField] LayerMask enemyMask;
+
+    // Rift Pulse (Q)
+    [SerializeField] int pulseDamage;
+    [SerializeField] float pulseRange;
+    [SerializeField] float pulseCooldown;
+
+    // Rift Surge (E)
+    [SerializeField] float surgeDuration;
+    [SerializeField] float surgeSpeedBoost;
+    [SerializeField] float surgeDamageBoost;
+    [SerializeField] float surgeCooldown;
+
+    // Rift Collapse (R)
+    [SerializeField] int collapseDamage;
+    [SerializeField] float collapseRadius;
+    [SerializeField] float collapseCooldown;
+    [SerializeField] float collapseSlowTime;
+    [SerializeField] float collapseSlowScale;
+
+    // Rift Jump (F)
+    [SerializeField] float jumpDistance;
+    [SerializeField] float jumpCooldown;
+
+    // Timers
+    float pulseTimer;
+    float surgeTimer;
+    float collapseTimer;
+    float jumpTimer;
+
+    // Surge state
+    bool isSurging;
+    float surgeEndTime;
+    float originalSpeed;
+    float originalDamageBoost = 1f;
+
+    void Start()
+    {
+        if (controller == null)
+            controller = GetComponent<playerController>();
+
+        originalSpeed = playerController.controller.speed;
+    }
+
+    void Update()
+    {
+        pulseTimer += Time.deltaTime;
+        surgeTimer += Time.deltaTime;
+        collapseTimer += Time.deltaTime;
+        jumpTimer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Q) && pulseTimer >= pulseCooldown)
+            StartCoroutine(RiftPulse());
+
+        if (Input.GetKeyDown(KeyCode.E) && surgeTimer >= surgeCooldown)
+            StartCoroutine(RiftSurge());
+
+        if (Input.GetKeyDown(KeyCode.R) && collapseTimer >= collapseCooldown)
+            StartCoroutine(RiftCollapse());
+
+        if (Input.GetKeyDown(KeyCode.F) && jumpTimer >= jumpCooldown)
+            RiftJump();
+
+        if (isSurging && Time.time >= surgeEndTime)
+            EndSurge();
+    }
+
+    // Q – short-range burst
+    IEnumerator RiftPulse()
+    {
+        pulseTimer = 0;
+        Collider[] hits = Physics.OverlapSphere(transform.position, pulseRange, enemyMask);
+        foreach (Collider hit in hits)
+        {
+            IDamage dmg = hit.GetComponent<IDamage>();
+            if (dmg != null)
+                dmg.takeDamage(pulseDamage);
+        }
+        yield return null;
+    }
+
+    // E – temporary boost
+    IEnumerator RiftSurge()
+    {
+        surgeTimer = 0;
+        isSurging = true;
+        surgeEndTime = Time.time + surgeDuration;
+
+        controller.speed = originalSpeed * surgeSpeedBoost;
+        controller.damageBoost = surgeDamageBoost;
+
+        yield return new WaitForSeconds(surgeDuration);
+        EndSurge();
+    }
+
+    void EndSurge()
+    {
+        isSurging = false;
+        controller.speed = originalSpeed;
+        controller.damageBoost = originalDamageBoost;
+    }
+
+    // R – heavy AoE + slow-mo
+    IEnumerator RiftCollapse()
+    {
+        collapseTimer = 0;
+        Collider[] hits = Physics.OverlapSphere(transform.position, collapseRadius, enemyMask);
+        foreach (Collider hit in hits)
+        {
+            IDamage dmg = hit.GetComponent<IDamage>();
+            if (dmg != null)
+                dmg.takeDamage(collapseDamage);
+        }
+
+        Time.timeScale = collapseSlowScale;
+        yield return new WaitForSecondsRealtime(collapseSlowTime);
+        Time.timeScale = 1f;
+    }
+
+    // F – teleport forward
+    void RiftJump()
+    {
+        jumpTimer = 0;
+        RaycastHit hit;
+        Vector3 targetPos = transform.position + transform.forward * jumpDistance;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, jumpDistance))
+            targetPos = hit.point - transform.forward * 1f;
+
+        controller.GetComponent<CharacterController>().enabled = false;
+        transform.position = targetPos;
+        controller.GetComponent<CharacterController>().enabled = true;
+    }
+}
